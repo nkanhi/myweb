4ccbd76269079f9e9c5fc8941b936ba9323539e3
Ái Nhi <ainhi191910@gmail.com>
2022-03-19 22:20:16 +0700

    update code project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    
+        <form>
+        
+          <label >
+            <input type="text" class="cls" />
+            <input type="submit" class="btn" /><br></br>
+          
+          </label>
+        
+          <div class="myDiv">
+              <div class="scroll"></div></div>
+      <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+      <div id="myDropdown" class="dropdown-content">
+        <a href="#home">1</a>
+        <a href="#about">2</a>
+        <a href="#contact">3</a>
+      </div>
+          
+          <br></br>
+          
+        <br></br>
+          <label >
+            <input type="text" class="cls" />
+            <input type="submit" class="btn"/>
+          </label>
+        </form>
+        
+      );
+    }
+
+export default App;
